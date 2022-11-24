0241c806da2fc81b4b3ebd0d9f366ad86a0776e4
Mike McKague <m.mckague19@gmail.com>
2022-11-24 11:10:57 -0500

    Added _insert_disks_at_points_m method for magnet

--- porespy/tools/_sphere_insertions.py
+++ porespy/tools/_sphere_insertions.py
@@ -9,6 +9,7 @@ __all__ = [
     '_make_balls',
     '_insert_disk_at_points',
     '_insert_disks_at_points',
+    '_insert_disks_at_points_m'
 ]
 
 
@@ -183,6 +184,72 @@ def _insert_disks_at_points(im, coords, radii, v, smooth=True,
                                         if overwrite or (im[x, y, z] == 0):
                                             im[x, y, z] = v
     return im
+
+
+@numba.jit(nopython=True, parallel=False)
+def _insert_disks_at_points_m(im, coords, radii, v, smooth=True,
+                              overwrite=False):  # pragma: no cover
+    r"""
+    Insert spheres (or disks) of specified radii into an ND-image at given locations.
+
+    This function uses numba to accelerate the process, and does not overwrite
+    any existing values (i.e. only writes to locations containing zeros).
+
+    Parameters
+    ----------
+    im : ND-array
+        The image into which the spheres/disks should be inserted. This is an
+        'in-place' operation.
+    coords : ND-array
+        The center point of each sphere/disk in an array of shape
+        ``ndim by npts``
+    radii : array_like
+        The radii of the spheres/disks to add.
+    v : scalar
+        The value to insert
+    smooth : boolean, optional
+        If ``True`` (default) then the spheres/disks will not have the litte
+        nibs on the surfaces.
+    overwrite : boolean, optional
+        If ``True`` then the inserted spheres overwrite existing values.  The
+        default is ``False``.
+
+    """
+    p_coords = []
+    npts = len(coords[0])
+    if im.ndim == 2:
+        xlim, ylim = im.shape
+        for i in range(npts):
+            r = radii[i]
+            s = _make_disk(r, smooth)
+            pt = coords[:, i]
+            if im[pt[0], pt[1]] == 0:
+                p_coords.append(pt)
+                for a, x in enumerate(range(pt[0]-r, pt[0]+r+1)):
+                    if (x >= 0) and (x < xlim):
+                        for b, y in enumerate(range(pt[1]-r, pt[1]+r+1)):
+                            if (y >= 0) and (y < ylim):
+                                if s[a, b] == 1:
+                                    if overwrite or (im[x, y] == 0):
+                                        im[x, y] = v[i]
+    elif im.ndim == 3:
+        xlim, ylim, zlim = im.shape
+        for i in range(npts):
+            r = radii[i]
+            s = _make_ball(r, smooth)
+            pt = coords[:, i]
+            if im[pt[0], pt[1], pt[2]] == 0:
+                p_coords.append(pt)
+                for a, x in enumerate(range(pt[0]-r, pt[0]+r+1)):
+                    if (x >= 0) and (x < xlim):
+                        for b, y in enumerate(range(pt[1]-r, pt[1]+r+1)):
+                            if (y >= 0) and (y < ylim):
+                                for c, z in enumerate(range(pt[2]-r, pt[2]+r+1)):
+                                    if (z >= 0) and (z < zlim):
+                                        if s[a, b, c] == 1:
+                                            if overwrite or (im[x, y, z] == 0):
+                                                im[x, y, z] = v[i]
+    return im, p_coords
 
 
 @numba.jit(nopython=True, parallel=False)
